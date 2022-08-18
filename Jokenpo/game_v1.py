de915fe837ca8cd9c94d22c053843d1abeb458cf
Luana Nunes <lununes.s@outlook.com>
2022-08-18 13:18:13 -0300

    Update game Jokenpo

--- Jokenpo/game_v1.py
+++ Jokenpo/game_v1.py
@@ -4,6 +4,7 @@ itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
 print('\nSuas opções: \n[ 0 ] PEDRA \n[ 1 ] PAPEL \n[ 2 ] TESOURA')
 jogador = int(input('Qual é a sua jogada? '))
+
 print('JO')
 print('KEN')
 print('PO!!!')
@@ -11,4 +12,19 @@ print('-=' * 18)
 print(f'Computador jogou {itens[computador]}.')
 print(f'Jogador jogou {itens[jogador]}.')
 print('-=' * 18)
+print('\n')
 
+if itens[computador] == itens[jogador]:
+    print('Empate!\n')
+elif itens[computador] == 'Pedra' and itens[jogador] == 'Tesoura':
+    print ('O Computador é o vencedor!\n')
+elif itens[computador] == 'Pedra' and itens[jogador] == 'Papel':
+    print ('O Jogador é o vencedor!\n')
+elif itens[computador] == 'Tesoura' and itens[jogador] == 'Papel':
+    print ('O Computador é o vencedor!\n')
+elif itens[computador] == 'Tesoura' and itens[jogador] == 'Pedra':
+    print ('O Jogador é o vencedor!\n')
+elif itens[computador] == 'Papel' and itens[jogador] == 'Pedra':
+    print ('O Computador é o vencedor!\n')
+elif itens[computador] == 'Papel' and itens[jogador] == 'Tesoura':
+    print ('O Jogador é o vencedor!\n')
